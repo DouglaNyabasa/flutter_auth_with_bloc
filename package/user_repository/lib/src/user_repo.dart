afcd1e9e9651a4572fcec22bba5943c8a95cc4a8
douglanyabasa <douglasnyabasa400@gmail.com>
2026-03-03 14:28:55 +0200

    user abstract repo class completed

---
+++ package/user_repository/lib/src/user_repo.dart
@@ -0,0 +1,10 @@
+import 'package:user_repository/src/models/models.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class UserRepository {
+
+  Stream<User?> get user;
+  Future<MyUser> signUp(MyUser myUser,String password);
+  Future<void> setUserData(MyUser user);
+  Future<void> signIn(String email,String password);
+}
